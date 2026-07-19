a3b1c1a262a5ce98d44fbc65f1556c4a957f218c
Chanwoo Park <148884409+Lymonada@users.noreply.github.com>
2026-07-19 23:57:22 +0900

    Add data loading for linear regression

--- pytorch_basics/01_linear_regression.py
+++ pytorch_basics/01_linear_regression.py
@@ -1,1 +1,11 @@
+import numpy as np
+from pathlib import Path
+from torch import nn
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+data_path = BASE_DIR / "data" / "pokemon.csv"
+
+loaded_data = np.loadtxt(datapath)
+
+x_train_np = loaded_data[:, 0:-1]
+y_train_np = loaded_data[:, [-1]]
