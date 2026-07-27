422bd608fac5265456ee7f01761c31f8df587756
Chanwoo Park <148884409+Lymonada@users.noreply.github.com>
2026-07-27 20:21:43 +0900

    Update 01_linear_regression.py

--- pytorch_basics/01_linear_regression.py
+++ pytorch_basics/01_linear_regression.py
@@ -1,11 +1,12 @@
 import numpy as np
+import torch
 from pathlib import Path
 from torch import nn
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 data_path = BASE_DIR / "data" / "linear_regression_data.csv"
 
-loaded_data = np.loadtxt(datapath)
+loaded_data = np.loadtxt(data_path, delimiter = ",")
 
 x_train_np = loaded_data[:, 0:-1]
 y_train_np = loaded_data[:, [-1]]
@@ -16,7 +17,7 @@ y_train = torch.Tensor(y_train_np)
 class MyLinearRegressionModel(nn.Module):
 
     def __init__(self, input_nodes):
-        super.init()
+        super.__init__()
         self.linear_stack = nn.Sequential(
             nn.Linear(input_nodes, 1)
         )
@@ -25,14 +26,14 @@ class MyLinearRegressionModel(nn.Module):
         prediction = self.linear_stack(data)
         return prediction
 
-model = MyLinearRegressionModel(loaded_data)
+model = MyLinearRegressionModel(x_train.shape[1])
 
 for name, child in model.named_children():
     for param in child.parameters():
         print(name, param)
 
 loss_function = nn.MSELoss()
-optimizer = torch.optim.SGD()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
 
 loss_list = []
 nums_epoch = 2000
